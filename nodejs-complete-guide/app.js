31b0e0809f746669f337c157102e609482d551a4
joconstantine <joconstantine@users.noreply.github.com>
2021-06-10 23:38:39 +0800

    10/06/2021 23:38

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -1,7 +1,29 @@
 const http = require('http');
-const routes = require('./routes');
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+})
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware!');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(3000)
