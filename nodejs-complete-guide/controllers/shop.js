0faace7d406ada459a9136607f42e1d5d2f0d1de
joconstantine <joconstantine@users.noreply.github.com>
2021-06-30 19:26:30 +0800

    30/06/2021 19:26

--- nodejs-complete-guide/controllers/shop.js
+++ nodejs-complete-guide/controllers/shop.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const rootDir = require('../util/path');
 const path = require('path');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const PDFDocument = require('pdfkit');
 
@@ -134,7 +135,45 @@ exports.postCartDeleteProduct = (req, res, next) => {
         });
 }
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+    let products;
+    let total = 0;
+    req.user
+        .populate('cart.items.productId')
+        .execPopulate()
+        .then(user => {
+            products = user.cart.items;
+            products.forEach(p => {
+                total += p.quantity * p.productId.price;
+            });
+
+            return stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                line_items: products.map(p => {
+                    return {
+                        name: p.productId.title,
+                        description: p.productId.description,
+                        amount: p.productId.price * 100, // in cents
+                        currency: 'usd',
+                        quantity: p.quantity,
+                    };
+                }),
+                success_url: req.protocol + '://' + req.get('host') + '/checkout/success', //http://localhost:3000/checkout/success
+                cancel_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+            });
+        })
+        .then(session => {
+            res.render('shop/checkout', {
+                path: '/checkout',
+                pageTitle: 'Checkout',
+                products: products,
+                totalSum: total,
+                sessionId: session.id,
+            });
+        });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
     req.user.populate('cart.items.productId')
         .execPopulate()
         .then(user => {
@@ -181,13 +220,6 @@ exports.getOrders = (req, res, next) => {
             error.httpStatus = 500;
             return next(error);
         });
-};
-
-exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        path: '/checkout',
-        pageTitle: 'Checkout',
-    });
 };
 
 exports.getInvoice = (req, res, next) => {
@@ -240,6 +272,4 @@ exports.getInvoice = (req, res, next) => {
             }
         })
         .catch(err => next(err));
-
-
 }
