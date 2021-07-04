37acaeca7c9a0dc0e8cd81dcc839cb42bc8df148
joconstantine <joconstantine@users.noreply.github.com>
2021-07-04 22:42:54 +0800

    04/07/2021 22:43

--- nodejs-complete-guide-rest/app.js
+++ nodejs-complete-guide-rest/app.js
@@ -1,14 +1,38 @@
+const path = require('path');
+
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 const MONGODB_URL = 'mongodb+srv://joconstantine:' + process.env.MONGO_DB_PASSWORD + '@cluster0.d1bhv.mongodb.net/messages?retryWrites=true&w=majority'
 
 // app.use(express.urlencoded()); // x-www-form-urlencoded <form>
 app.use(express.json());
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,6 +42,13 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
 
 mongoose.connect(MONGODB_URL)
     .then(result => {
